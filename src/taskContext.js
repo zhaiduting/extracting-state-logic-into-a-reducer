b4134253a3347f1f12393ba2db22f4de43f6d475
zhaiduting <zhaiduting@163.com>
2024-10-14 15:41:16 +0800

    No need to use Reducer

--- src/taskContext.js
+++ src/taskContext.js
@@ -1,7 +1,7 @@
 import {createContext} from "react";
-import {taskReducer} from "./taskReducer";
-import {useImmerReducer} from "use-immer";
+import {useImmer} from "use-immer";
 
+let nextId = 3
 const initialTasks = [
     {id: 0, text: '参观卡夫卡博物馆', done: true},
     {id: 1, text: '看木偶戏', done: false},
@@ -12,19 +12,27 @@ export const TaskContext = createContext();
 const Provider = TaskContext.Provider
 
 TaskContext.Provider = function ({children}) {
-    const [tasks, dispatch] = useImmerReducer(taskReducer, initialTasks);
+    const [tasks, setTasks] = useImmer(initialTasks)
 
     return <Provider value={
         {
             tasks,
             handleAddTask(task) {
-                dispatch({type: 'addTask', task})
+                setTasks(drafts => {
+                    drafts.push({...task, id: nextId++})
+                });
             },
             handleUpdateTask(task) {
-                dispatch({type: 'updateTask', task})
+                setTasks(drafts => {
+                    const index = drafts.findIndex((draft) => draft.id === task.id)
+                    drafts[index] = task
+                })
             },
             handleDeleteTask(task) {
-                dispatch({type: 'deleteTask', task})
+                setTasks(drafts => {
+                    const index = drafts.findIndex((draft) => draft.id === task.id)
+                    drafts.splice(index, 1)
+                })
             }
         }
     }>{children}</Provider>;
